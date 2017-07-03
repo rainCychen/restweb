67c9c086b222cf933830329d39856ccd151b93c5
rainCychen <cychenPHP@163.com>
2017-07-03 15:57:10 +0800

    注释+表单

--- src/controller/test/test.go
+++ src/controller/test/test.go
@@ -31,7 +31,7 @@ func (w *Web) InitRoute(Container *restful.Container) {
 	ws.Route(ws.GET("/index").To(w.handleIndex))
 	ws.Route(ws.GET("/login").To(w.handleForm))
 	ws.Route(ws.POST("/login").Consumes("application/x-www-form-urlencoded").To(w.handleLogin))
-	ws.Route(ws.GET("/user").To(w.handleForm))
+	ws.Route(ws.GET("/user").To(w.handleUserForm))
 	ws.Route(ws.POST("/user").Consumes("application/x-www-form-urlencoded").To(w.handleUser))
 	ws.Route(ws.GET("/html").To(w.handleHtml))
 	Container.Add(ws)
@@ -48,6 +48,9 @@ type Profile struct {
 	Age  int
 }
 
+/*
+简单的信息打印（io.WriteString(rsp, "小明")）
+*/
 func (w *Web) handleTest(req *restful.Request, rsp *restful.Response) {
 	info := Info{
 		Id:   1,
@@ -60,6 +63,10 @@ func (w *Web) handleTest(req *restful.Request, rsp *restful.Response) {
 	m["list"] = info
 	io.WriteString(rsp, "小明")
 }
+
+/*
+简单的信息打印（rsp.WriteEntity(info)）
+*/
 func (w *Web) handleIndex(req *restful.Request, rsp *restful.Response) {
 	info := Info{
 		Id:   1,
@@ -68,6 +75,10 @@ func (w *Web) handleIndex(req *restful.Request, rsp *restful.Response) {
 	}
 	rsp.WriteEntity(info)
 }
+
+/*
+表单数据
+*/
 func (w *Web) handleLogin(req *restful.Request, rsp *restful.Response) {
 	err := req.Request.ParseForm()
 	if err != nil {
@@ -83,15 +94,10 @@ func (w *Web) handleLogin(req *restful.Request, rsp *restful.Response) {
 	}
 	io.WriteString(rsp.ResponseWriter, fmt.Sprintf("<html><body>Name=%s, Age=%d</body></html>", p.Name, p.Age))
 }
-func (w *Web) handleUser(req *restful.Request, rsp *restful.Response) {
-	p, err := url.QueryUnescape(req.Request.URL.RawQuery)
-	if err != nil {
-		rsp.WriteErrorString(http.StatusBadRequest, err.Error())
-		return
-	}
-	fmt.Printf("%T(%[1]v)", p)
-	io.WriteString(rsp, p)
-}
+
+/*
+表单
+*/
 func (w *Web) handleForm(req *restful.Request, rsp *restful.Response) {
 	io.WriteString(rsp,
 		`<html>
@@ -118,4 +124,24 @@ func (w *Web) handleHtml(req *restful.Request, rsp *restful.Response) {
 		log.Fatalf("Template gave: %s", err)
 	}
 	t.Execute(rsp.ResponseWriter, nil)
+}
+func (w *Web) handleUserForm(req *restful.Request, rsp *restful.Response) {
+	t, err := template.ParseFiles(StaicHtml + "user.html")
+	if err != nil {
+		log.Fatalf("Template gave: %s", err)
+	}
+	t.Execute(rsp.ResponseWriter, nil)
+}
+
+/*
+获取表单get中的数据
+*/
+func (w *Web) handleUser(req *restful.Request, rsp *restful.Response) {
+	p, err := url.QueryUnescape(req.Request.URL.RawQuery)
+	if err != nil {
+		rsp.WriteErrorString(http.StatusBadRequest, err.Error())
+		return
+	}
+	fmt.Printf("%T(%[1]v)", p)
+	io.WriteString(rsp, fmt.Sprintf("GET:%T(%[1]v)", p))
 }

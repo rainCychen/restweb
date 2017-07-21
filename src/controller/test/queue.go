1906e7ad58f104334d19dba13cc9dddee78e9d21
rainCychen <cychenPHP@163.com>
2017-07-21 18:02:01 +0800

    json
    json.Marshal的参数struct需要`json:"a"`形式

--- src/controller/test/queue.go
+++ src/controller/test/queue.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Queue struct {
-	a string
-	b int
-	c bool
+	A string `json:"a"`
+	B int    `json:"b"`
+	C bool   `json:"c"`
 }
 
 /* 用redis发送消息 */
@@ -17,12 +17,13 @@ func (w *Web) Sync(a string, b int, c bool) error {
 	conn := w.redisPool.Get()
 	defer conn.Close()
 	//后端绘制队列
-	qm := &Queue{
-		a: a,
-		b: b,
-		c: c,
+	qm := Queue{
+		A: a,
+		B: b,
+		C: c,
 	}
 	buf, _ := json.Marshal(qm)
+	glog.Infoln(buf, qm)
 	_, err := conn.Do("LPUSH", "test", buf)
 	if err != nil {
 		glog.Warningln("LPUSH fail", err)
